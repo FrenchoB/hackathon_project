9c8181d79b5036462a748c6f0296570187533692
Agathe Boncompain <agathe.boncompain@gmail.com>
2025-08-05 10:22:28 +0200

    validation des données avec joi

--- models/User.js
+++ models/User.js
@@ -1,48 +1,51 @@
 import mongoose from "mongoose";
 //Definition du modèle de données pour les utilisateurs
-const userSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.ObjectId, auto: true },
-  username: {
-    type: String,
-    required: [true, "le nom d'utilisateur est requis"],
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "le nom d'utilisateur est requis"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Email invalide"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "le mot de passe doit comporter au moins 6 caractères"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    accessibilité: {
+      type: String,
+      enum: [
+        "Sourd",
+        "Aveugle",
+        "Sourd-Muet",
+        "Malvoyant",
+        "Malentendant",
+        "Daltonien",
+        "Non défini",
+      ],
+      default: "Non défini",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+\@.+\..+/, "Email invalide"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: [3, "le mot de passe doit comporter au moins 3 caractères"],
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  accessibilité: {
-    type: String,
-    enum: [
-      "Sourd",
-      "Aveugle",
-      "Sourd-Muet",
-      "Malvoyant",
-      "Malentendant",
-
-      "Daltonien",
-      "Non défini",
-    ],
-    default: "Non défini",
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 //Création du modèle User
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
---
+++ routes/user.routes.js
@@ -0,0 +1,3 @@
+import { createUserSchema } from "../validations/user.validation.js";
+import { validate } from "../middlewares/validator.js";
+
