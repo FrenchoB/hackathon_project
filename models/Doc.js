946b073b20ab8344a264697f8b6093c17f951622
PhiB64 <philippebarbosa64@gmail.com>
2025-08-06 18:03:08 +0200

    feat: ajout du module 'say' pour message vocal lors de la création de document

--- models/Doc.js
+++ models/Doc.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 //Definition du modèle de données pour les documents
 const documentSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true, unique: true },
-  description: { type: String, unique: true, required: true },
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true },
   format: {
     type: String,
     enum: ["text", "image", "audio", "video", "epub", "pdf"],
     required: true,
   },
-  file: { type: String, required: true, unique: true },
+  file: { type: String, required: true },
   public_id: { type: String, required: true },
   tags: [{ type: String, trim: true, lowercase: true, required: true }],
 
