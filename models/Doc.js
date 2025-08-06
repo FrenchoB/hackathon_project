fe7979e35a2423d073caae30446bbc3d8b81153d
PhiB64 <philippebarbosa64@gmail.com>
2025-08-06 11:31:34 +0200

    Fusion de origin/master dans docs-philippe — mise à jour du modèle Doc.js

--- models/Doc.js
+++ models/Doc.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //Definition du modèle de données pour les documents
 const documentSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, unique: true },
-  description: { type: String, unique: true },
+  description: { type: String, unique: true, required: true },
   format: {
     type: String,
     enum: ["text", "image", "audio", "video", "epub", "pdf"],
@@ -11,7 +11,7 @@ const documentSchema = new mongoose.Schema({
   },
   file: { type: String, required: true, unique: true },
   public_id: { type: String, required: true },
-  tags: [{ type: String, trim: true, lowercase: true }],
+  tags: [{ type: String, trim: true, lowercase: true, required: true }],
 
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
